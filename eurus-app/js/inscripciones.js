85946b7477fcd290b90cfc98af57ab4d793442c7
Jesus41844 <Jesus.degracia831@gmail.com>
2026-06-24 20:13:45 -0500

    Correcciones finales finales

--- eurus-app/js/inscripciones.js
+++ eurus-app/js/inscripciones.js
@@ -331,7 +331,6 @@ function filaAInscripcion(fila, mapeo) {
 function validarInscripcion(ins) {
   if (!ins.nombre)  return "Nombre vacío";
   if (!ins.correo)  return "Correo vacío";
-  if (!ins.cedula)  return "Cédula vacía";
   return null;
 }
 
